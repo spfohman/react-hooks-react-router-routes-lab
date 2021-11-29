53202901ac8e28bf99b81265af4149709443ae79
+ <+>
2021-11-29 13:38:36 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,26 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  console.log(movies.title);
+  const eachMovie = movies.map((movie) => {
+    return (
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Runtime: {movie.time}</p>
+        <ul>
+          {movie.genres.map((genre) => (
+            <li key={genre}>{genre}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  });
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {eachMovie}
+    </div>
+  );
 }
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
